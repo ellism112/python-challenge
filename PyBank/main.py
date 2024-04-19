6d4dc9768ff81fb73d1ec427f2e1b45958b66511
ellism112 <162396128+ellism112@users.noreply.github.com>
2024-04-18 21:37:37 -0400

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,22 +5,45 @@ csvpath = os.path.join('PyBank', 'budget_data.csv')
 
 with open (csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    #print(csvreader)
-
     csv_header = next(csvreader)
-    #print(f"CSV header: {csv_header}")
     
     TotalMonths = 0
-    proloss = 0
+    ProLossTotal = 0
+    change = 0
+    months = []
+    profit = []
+    
+    FirstRow = next(csvreader)
+    TotalMonths += 1
+    ProLossTotal += int(FirstRow[1])
+    ProLoss = int(FirstRow[1])
+
     for row in csvreader:
-        #print(row)
-        TotalMonths =  TotalMonths + 1
-        proloss += int(row[1])
+        months.append(row[0])
+        change = int(row[1]) - ProLoss
+        profit.append(change)
+        ProLoss = int(row[1])
+        TotalMonths += 1
+        ProLossTotal = ProLossTotal +int(row[1])
+
+    Increase = max(profit)
+    GreatIndex = profit.index(Increase)
+    GreatMonth = months[GreatIndex]
+
+    Decrease = min(profit)
+    LeastIndex = profit.index(Decrease)
+    LeastMonth = months[LeastIndex]
+
+    average = sum(profit)/len(profit)
+
         
 print("Financial Analysis")
 print("-"*30)
 print("Total Months: ", TotalMonths)
-print("Total: $", proloss)
+print("Total: $", ProLossTotal)
+print("Average Change: $", round(average,2))
+print("Greatest Increase in Profits: ", GreatMonth, "($", Increase, ")")
+print("Greatest Descrease in Profits: ", LeastMonth, "($", Decrease, ")")
         
     
         
